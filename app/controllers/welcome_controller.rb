68df840ca8c926bda33b0d7bd9855ef26d812bc0
aeterne <aeterne@gmail.com>
2017-01-24 22:46:11 +0100

    Built out navigation and added welcome controller and root

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,19 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def pricing
+  end
+
+  def features
+  end
+
+  def faqs
+  end
+
+  def contact
+  end
+end
